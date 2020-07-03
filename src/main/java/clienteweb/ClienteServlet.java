28e8a5885605421f7eb05c4149992000d3bf7020
willian14 <willian.bandeira.dossantos@hotmail.com>
2020-07-02 21:03:30 -0400

    versao1

---
+++ src/main/java/clienteweb/ClienteServlet.java
@@ -0,0 +1,24 @@
+package clienteweb;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+				
+@WebServlet(urlPatterns = { "/cliente.do", "/clienteServlet" })
+public class ClienteServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método Get");
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.getWriter().print("Chamou pelo método Post");
+				
+	}
+}
